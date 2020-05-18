2f3f1e68e171354fc219503a857a9ecac70c12e7
User Sample <sample@bsd7.fizmat.vspu.ru>
2020-05-18 21:01:12 +0300

    First version 0.1

---
+++ task-randomizer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Task Randomizer
+ * Description: Составление базы заданий и предъявление задания обучающемуся
+ * Plugin URI:  https://github.com/alexey-sergeev/task-randomizer
+ * Author:      Alexey N. Sergeev
+ * Version:     0.1
+ */
+
+
+ print_r( 'Hello World!' );
+
+
+
+?>
