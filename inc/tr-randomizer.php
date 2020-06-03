96660bdd3d91367d44eb0875b2c819299b096378
User Sample <sample@bsd7.fizmat.vspu.ru>
2020-06-03 21:57:27 +0300

    v 0.7 history

--- inc/tr-randomizer.php
+++ inc/tr-randomizer.php
@@ -29,15 +29,72 @@ class tr_randomizer extends tr_core {
     {
         $arr2 = array();
 
+        if ( $param['history'] == 'history' ) {
+            
+            $arr2 = $this->get_history( $arr, $param );
+
+            if ( $arr2 ) return $arr2;
+            
+        }
+        
+
         if ( $param['choice'] == 'random' ) {
 
             shuffle( $arr );
             $arr2 = array_slice( $arr, 0, $param['num'] );
 
+            if ( $param['history'] == 'history' ) $this->set_history( $arr2 );
 
         }
 
         return $arr2;
+    }
+    
+
+    
+    // 
+    // Извлечь задачи из истории
+    // 
+
+    public function get_history( $arr, $param )
+    {
+        global $post;
+        
+        if ( ! is_user_logged_in() ) return false;
+
+        $user_id = get_current_user_id();
+
+        $arr2 = get_post_meta( $post->ID, 'task-history-' . $user_id );
+
+        $ret = false;
+
+        foreach ( $arr2 as $item ) {
+
+            $arr3 = array_intersect( $arr, $item );
+            if ( count( $arr3 ) == $param['num'] ) $ret = $arr3;
+
+        }
+
+        return $ret;
+    }
+
+
+
+    // 
+    // Записать в историю выбора
+    // 
+
+    public function set_history( $arr )
+    {
+        global $post;
+        
+        if ( ! is_user_logged_in() ) return false;
+
+        $user_id = get_current_user_id();
+
+        $ret = add_post_meta( $post->ID, 'task-history-' . $user_id, $arr );
+        
+        return $ret;
     }
     
 
